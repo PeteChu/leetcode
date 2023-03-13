accc0d19978344ca52bebf240b7452d1759aee55
PeteChu <phuengton.ch@gmail.com>
2023-03-13 11:52:04 +0700

    * refactor(1137_N-th_Tribonacci_Number.py): remove unnecessary whitespace before return statement

--- 1137_N-th_Tribonacci_Number.py
+++ 1137_N-th_Tribonacci_Number.py
@@ -9,7 +9,7 @@ class Solution:
         if n == 0:
             return 0
         if n < 3:
-            return 1 
+            return 1
         if n not in self.mem:
             self.mem[n] = self.tribonacci(n -1 ) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
         return self.mem[n]
